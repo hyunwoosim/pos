33acb42834139684fc9477bef262b1e424deab5c
hyunwoo <kshsim98@naver.com>
2024-12-12 06:17:33 +0900

    Initial commit

---
+++ src/test/java/com/mulook/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulook.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
