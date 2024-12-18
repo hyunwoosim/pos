1720a86c7e058b885089cc198ee50a425620813f
hyunwoo <kshsim98@naver.com>
2024-12-19 01:12:22 +0900

    aws s3를 연동하여 이미지 처리 완료

--- src/main/java/com/mulook/pos/controller/ItemController.java
+++ src/main/java/com/mulook/pos/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.mulook.pos.controller;
 
 import com.mulook.pos.Service.ItemService;
 import com.mulook.pos.Service.ValidateHandlingService;
+import com.mulook.pos.Service.aws.S3Service;
 import com.mulook.pos.dto.ItemDto;
 import com.mulook.pos.entity.ItemType;
 import jakarta.validation.Valid;
@@ -12,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +22,7 @@ public class ItemController {
 
     private final ItemService itemService;
     private final ValidateHandlingService validateHandlingService;
+    private final S3Service s3Service;
 
 
     @GetMapping("/item/new")
@@ -48,4 +52,14 @@ public class ItemController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/presigned-url")
+    @ResponseBody
+    String getURL(@RequestParam String filename) {
+        String result = s3Service.createPresignedUrl("menu/" + filename);
+        System.out.println("result = " + result);
+        return result;
+    }
+
+
 }
