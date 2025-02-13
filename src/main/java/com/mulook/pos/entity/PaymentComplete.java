6603fedeaed1d9429723908cfe5a146bfd0bd0ac
hyunwoo <kshsim98@naver.com>
2025-02-14 07:37:10 +0900

    paymentsComplete엔티티 구상중

---
+++ src/main/java/com/mulook/pos/entity/PaymentComplete.java
@@ -0,0 +1,16 @@
+package com.mulook.pos.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentComplete {
+
+}
